5940511dd7d10c56c66d8afc7fa3d3656090b830
haneeth-accelance-9 <haneeth.dabbiru.ext@bayer.com>
2026-03-30 12:46:10 +0000

    Changes for Export button and Profile new page created

--- app/optimizer/urls.py
+++ app/optimizer/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("login/", views.OptimizerLoginView.as_view(), name="login"),
     path("signup/", views.signup_view, name="signup"),
     path("logout/", views.logout_view, name="logout"),
+    path("profile/", views.profile_page, name="profile"),
     path("upload/", views.upload, name="upload"),
     path("results/", views.results, name="results"),
     path("dashboard/", views.dashboard, name="dashboard"),
